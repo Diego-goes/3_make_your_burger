d79812acebc044698070a11007e475e3b03ca60b
Diego Goes <diego@vsktechnology.com.br>
2022-09-05 19:35:17 -0300

    Configurações do View Router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <router-link to="/">
+        <img src="../../public/img/logo.png">
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedidos">Pedidos</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+}
+</script>
+
+<style scoped>
+
+</style>
