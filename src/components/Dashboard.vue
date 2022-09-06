da1038ca5990664f506ad19e6e5380afaaaac4e5
Diego Goes <diego@vsktechnology.com.br>
2022-09-06 13:19:52 -0300

    Resgate de pedidos do banco

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,15 +11,14 @@
         </div>
     </div>
     <div id="burger-table-row">
-        <div class="burger-table-row">
-            <div class="order-number">1</div>
-            <div>João</div>
-            <div>Pão de trigo</div>
-            <div>Maminha</div>
+        <div class="burger-table-row" v-for="burger in burgers" :key="burger.id" >
+            <div class="order-number">{{burger.id}}</div>
+            <div>{{burger.nome}}</div>
+            <div>{{burger.pao}}</div>
+            <div>{{burger.carne}}</div>
             <div>
                 <ul>
-                    <li>Salame</li>
-                    <li>Tomate</li>
+                    <li v-for="(opcional, index) in burger.opcionais" :key="index" >{{opcional}}</li>
                 </ul>
             </div>
             <div>
@@ -53,7 +52,25 @@
 
 <script>
 export default {
-    name: "Dashboard"
+    name: "Dashboard",
+    data(){
+        return{
+            burgers: null,
+            burger_id: null,
+            status: []
+        }
+    },
+    methods: {
+        async getPedidos(){
+            const req = await fetch('http://localhost:3000/burgers')
+            const data = await req.json()
+            this.burgers = data
+            // Resgatar Status
+        }
+    },
+    mounted(){
+        this.getPedidos()
+    }
 }
 </script>
 
